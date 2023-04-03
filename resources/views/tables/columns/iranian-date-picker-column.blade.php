d180eb8ca52c83bfe659035bf9cd80a9976381ec
shayan <shayan.yousefi100@yahoo.com>
2023-04-03 14:12:33 +0330

    fix: converting date from gregorian to iranian date

--- resources/views/tables/columns/iranian-date-picker-column.blade.php
+++ resources/views/tables/columns/iranian-date-picker-column.blade.php
@@ -1,3 +1,11 @@
-<div x-text="new persianDate(@js($getState()->toDateString())).toLocale(@js(\Illuminate\Support\Facades\Config::get('app.locale'))).format(@js($getFormat()))">
+@php
+    $state = $getState();
+    if(is_string($getState())){
+        $state = \Carbon\Carbon::parse($state);
+    }
+@endphp
+
+<div
+    x-text="new persianDate(@js($state->valueOf())).toLocale(@js(\Illuminate\Support\Facades\Config::get('app.locale'))).format(@js($getFormat()))">
 
 </div>
