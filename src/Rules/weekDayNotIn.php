7913f12b5465cbbae9b7186ab421d4d4feacb588
shayan <shayan.yousefi100@yahoo.com>
2023-04-24 15:24:36 +0330

    validation to check week day not in disabled week day by disableWeekDay method

--- src/Rules/weekDayNotIn.php
+++ src/Rules/weekDayNotIn.php
@@ -2,11 +2,17 @@
 
 namespace ShayanYS\IranianDatePicker\Rules;
 
+use Carbon\Carbon;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 
 class weekDayNotIn implements ValidationRule
 {
+    public function __construct(private array $days = [])
+    {
+
+    }
+
     /**
      * Run the validation rule.
      *
@@ -14,6 +20,21 @@ class weekDayNotIn implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        //
+
+        $iranWeekDays = [
+          Carbon::SATURDAY => 0,
+          Carbon::SUNDAY => 1,
+          Carbon::MONDAY => 2,
+          Carbon::TUESDAY => 3,
+          Carbon::WEDNESDAY => 4,
+          Carbon::THURSDAY => 5,
+          Carbon::FRIDAY => 6,
+        ];
+
+        $valueWeekDay = $iranWeekDays[Carbon::parse($value)->dayOfWeek];
+
+        if(in_array($valueWeekDay,$this->days)){
+            $fail('IranianDatePicker::validation.weekDayNotIn')->translate();
+        }
     }
 }
